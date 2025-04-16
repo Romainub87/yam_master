011e2e88aa8b36096e558d491eef073088662272
LucasBout <106062684+LucasBout@users.noreply.github.com>
2025-04-16 10:24:59 +0200

    feat(ranked): add matchmaking based on mmr

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import { useAuth } from '@/context/AuthContext';
-import CustomButton from "@/components/CustomButton";
-import { useRouter } from "expo-router";
+import CustomButton from '@/components/CustomButton';
+import { useRouter } from 'expo-router';
 import { useWebSocket } from '@/context/WebSocketContext';
 
 export default function HomeScreen() {
@@ -17,10 +17,16 @@ export default function HomeScreen() {
         if (lastMessage.type === 'queue.added') {
           setIsSearching(true);
         } else if (lastMessage.type === 'game.start') {
-          router.push({ pathname: '/game', params: { id: lastMessage.game.id } });
+          router.push({
+            pathname: '/game',
+            params: { id: lastMessage.game.id },
+          });
         }
       } catch (error) {
-        console.error('Erreur lors du traitement du message WebSocket :', error);
+        console.error(
+          'Erreur lors du traitement du message WebSocket :',
+          error
+        );
       }
     }
   }, [lastMessage]);
@@ -46,23 +52,29 @@ export default function HomeScreen() {
   };
 
   return (
-      <View className="flex-1 justify-center items-center dark:bg-black">
-        {user ? (
-            <View className="items-center p-6 rounded-lg shadow-lg">
-              <Text className="text-4xl text-white mb-6 font-bold">Bienvenue, {user.username} 👋</Text>
-              {isSearching ? (
-                  <View className="items-center">
-                    <Text className="text-lg text-gray-300 mb-4">🔍 Recherche de parties en cours...</Text>
-                    <CustomButton title="Annuler" onPress={() => leaveQueue()} />
-                  </View>
-              ) : (
-                  <CustomButton title="Jouer" onPress={() => joinQueue()} />
-              )}
-              <CustomButton title="Se déconnecter" onPress={logout} />
+    <View className="flex-1 justify-center items-center dark:bg-black">
+      {user ? (
+        <View className="items-center p-6 rounded-lg shadow-lg">
+          <Text className="text-4xl text-white mb-6 font-bold">
+            Bienvenue, {user.username} 👋
+          </Text>
+          {isSearching ? (
+            <View className="items-center">
+              <Text className="text-lg text-gray-300 mb-4">
+                🔍 Recherche de parties en cours...
+              </Text>
+              <CustomButton title="Annuler" onPress={() => leaveQueue()} />
             </View>
-        ) : (
-            <Text className="text-lg text-gray-300">Veuillez vous connecter pour continuer</Text>
-        )}
-      </View>
+          ) : (
+            <CustomButton title="Jouer" onPress={() => joinQueue()} />
+          )}
+          <CustomButton title="Se déconnecter" onPress={logout} />
+        </View>
+      ) : (
+        <Text className="text-lg text-gray-300">
+          Veuillez vous connecter pour continuer
+        </Text>
+      )}
+    </View>
   );
 }
---
+++ backend/handlers/game.js
@@ -0,0 +1,272 @@
+import { getGameClients, addGameClient } from '../websocket.js';
+import db from '../connection.js';
+import { MessageTypes } from '../types/message.js';
+import jwt from 'jsonwebtoken';
+
+export async function handleGameSubscribe(client, payload) {
+  try {
+    const decodedToken = jwt.verify(payload.token, process.env.JWT_SECRET);
+    const userId = decodedToken.user.id;
+
+    // Récupérer les données de la partie en cours
+    const game = await db.game.findUnique({
+      where: { id: parseInt(payload.gameId, 10) },
+    });
+
+    if (!game) {
+      client.send(
+        JSON.stringify({
+          type: MessageTypes.GAME_ERROR,
+          message: 'Partie introuvable',
+        })
+      );
+      return;
+    }
+
+    const existingClient = getGameClients().find(
+      (gameClient) =>
+        gameClient.gameId === game.id && gameClient.userId === userId
+    );
+
+    if (!existingClient) {
+      addGameClient({ client, gameId: game.id, userId: userId });
+    }
+
+    const playerScores = await db.player_score.findMany({
+      where: { game_id: game.id },
+    });
+
+    const playerScore = playerScores.find(
+      (player) => player.user_id === userId
+    );
+    const opponentScore = playerScores.find(
+      (player) => player.user_id !== userId
+    );
+
+    // Envoyer les données de la partie au client
+    client.send(
+      JSON.stringify({
+        type: MessageTypes.GAME_UPDATE,
+        game: game,
+        playerScore: playerScore,
+        opponentScore: opponentScore,
+      })
+    );
+  } catch (error) {
+    console.error('Erreur dans handleGameSubscribe :', error);
+    client.send(
+      JSON.stringify({
+        type: MessageTypes.GAME_ERROR,
+        message: "Erreur lors de l'abonnement à la partie",
+      })
+    );
+  }
+}
+
+export async function handleRollDices(client, payload) {
+  const gameId = payload.gameId;
+  const decodedToken = jwt.verify(payload.token, process.env.JWT_SECRET);
+  const userId = decodedToken.user.id;
+
+  const playerScores = await db.player_score.findMany({
+    where: { game_id: gameId },
+  });
+
+  const playerScore = playerScores.find((player) => player.user_id === userId);
+  const opponentScore = playerScores.find(
+    (player) => player.user_id !== userId
+  );
+
+  if (!playerScore) {
+    client.send(
+      JSON.stringify({
+        type: MessageTypes.GAME_ERROR,
+        message: "Vous n'êtes pas dans cette partie",
+      })
+    );
+    return;
+  }
+
+  if (playerScore.rolls_left === 0) {
+    client.send(
+      JSON.stringify({
+        type: MessageTypes.GAME_ERROR,
+        message: 'Plus de lancers disponibles',
+      })
+    );
+    return;
+  }
+
+  if (!gameId) {
+    client.send(
+      JSON.stringify({
+        type: MessageTypes.GAME_ERROR,
+        message: 'ID de la partie manquant',
+      })
+    );
+    return;
+  }
+
+  const game = await db.game.findUnique({
+    where: { id: gameId },
+  });
+
+  if (!game) {
+    client.send(
+      JSON.stringify({
+        type: MessageTypes.GAME_ERROR,
+        message: 'Partie introuvable',
+      })
+    );
+    return;
+  }
+
+  const diceRolls = Array.from(
+    { length: payload.count },
+    () => Math.floor(Math.random() * 6) + 1
+  );
+
+  await db.player_score.update({
+    where: {
+      game_id_user_id: {
+        game_id: gameId,
+        user_id: userId,
+      },
+    },
+    data: {
+      rolls_left: {
+        decrement: 1,
+      },
+    },
+  });
+
+  const playerScoreUpdated = await db.player_score.findUnique({
+    where: {
+      game_id_user_id: {
+        game_id: gameId,
+        user_id: userId,
+      },
+    },
+  });
+
+  // Vérifier les combinaisons possibles
+  const counts = diceRolls.reduce((acc, value) => {
+    acc[value] = (acc[value] || 0) + 1;
+    return acc;
+  }, {});
+
+  // TODO: Vérifier les combinaisons possibles et les implémenter
+  const combinations = {
+    pair: Object.values(counts).some((count) => count === 2),
+    threeOfAKind: Object.values(counts).some((count) => count === 3),
+    fourOfAKind: Object.values(counts).some((count) => count === 4),
+    fullHouse:
+      Object.values(counts).includes(3) && Object.values(counts).includes(2),
+    yahtzee: Object.values(counts).some((count) => count === 5),
+    smallStraight:
+      [1, 2, 3, 4].every((num) => counts[num]) ||
+      [2, 3, 4, 5].every((num) => counts[num]) ||
+      [3, 4, 5, 6].every((num) => counts[num]),
+    largeStraight:
+      [1, 2, 3, 4, 5].every((num) => counts[num]) ||
+      [2, 3, 4, 5, 6].every((num) => counts[num]),
+  };
+
+  client.send(
+    JSON.stringify({
+      type: MessageTypes.DICE_ROLL,
+      dice: diceRolls,
+      game: game,
+      combinations: combinations,
+      playerScore: playerScoreUpdated,
+    })
+  );
+
+  if (opponentScore) {
+    const opponentClient = getGameClients().find(
+      (c) => c.gameId === gameId && c.userId === opponentScore.user_id
+    );
+    if (opponentClient) {
+      opponentClient.client.send(
+        JSON.stringify({
+          type: MessageTypes.OPPONENT_UPDATE,
+          dice: diceRolls,
+          game: game,
+          opponentScore: playerScoreUpdated,
+        })
+      );
+    }
+  }
+}
+
+export async function handleTurnChange(client, payload) {
+  const gameId = payload.gameId;
+
+  try {
+    // Récupérer les scores des joueurs pour la partie
+    const playerScores = await db.player_score.findMany({
+      where: { game_id: gameId },
+    });
+
+    if (playerScores.length !== 2) {
+      client.send(
+        JSON.stringify({
+          type: MessageTypes.GAME_ERROR,
+          message: 'Nombre de joueurs incorrect',
+        })
+      );
+      return;
+    }
+
+    // Inverser les tours
+    const updates = playerScores.map((player) => {
+      return db.player_score.update({
+        where: {
+          game_id_user_id: {
+            game_id: gameId,
+            user_id: player.user_id,
+          },
+        },
+        data: {
+          turn: !player.turn,
+          rolls_left: !player.turn ? 3 : 0,
+        },
+      });
+    });
+
+    await Promise.all(updates);
+
+    // Récupérer les scores mis à jour
+    const updatedScores = await db.player_score.findMany({
+      where: { game_id: gameId },
+    });
+
+    // Notifier les clients
+    updatedScores.forEach((player) => {
+      const gameClient = getGameClients().find(
+        (c) => c.gameId === gameId && c.userId === player.user_id
+      );
+      if (gameClient) {
+        gameClient.client.send(
+          JSON.stringify({
+            type: MessageTypes.GAME_UPDATE,
+            opponentScore: updatedScores.find(
+              (p) => p.user_id !== player.user_id
+            ),
+            playerScore: player,
+            game: { id: gameId }, // Inclure les données nécessaires de la partie
+            dice: [],
+          })
+        );
+      }
+    });
+  } catch (error) {
+    console.error('Erreur dans handleTurnChange :', error);
+    client.send(
+      JSON.stringify({
+        type: MessageTypes.GAME_ERROR,
+        message: 'Erreur lors du changement de tour',
+      })
+    );
+  }
+}
