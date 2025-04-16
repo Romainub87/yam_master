b16bba3434e8f10ea24865b3c8049d8a8acccc7d
lucas66166 <lucas66166@gmail.com>
2025-04-16 13:15:19 +0200

    feat: add cron for tryMatchPlayers

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 import http from 'http';
 import app from './app.js';
-import { setupWebSocket } from './websocket.js';
+import { setupWebSocket, getWaitingClients } from './websocket.js';
+import { tryMatchPlayers } from './lib/matchmaking.js';
 
 const PORT = 3000;
 
 const server = http.createServer(app);
 setupWebSocket(server);
+
+let matchingInProgress = false;
+setInterval(() => {
+  if (!matchingInProgress) {
+    matchingInProgress = true;
+    const waiting = getWaitingClients();
+    tryMatchPlayers(waiting);
+    matchingInProgress = false;
+  }
+}, 1000);
 
 server.listen(PORT, () => {
   console.log(`Serveur en ligne sur http://localhost:${PORT}`);
